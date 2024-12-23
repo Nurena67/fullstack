00cba01567e0be57808f079e01c8f3b7eaa30dcc
Nurena67 <arisnurena@gmail.com>
2024-12-23 23:30:02 +0700

    update file SignUp

--- frontend/src/pages/auth/signup/SignUp.js
+++ frontend/src/pages/auth/signup/SignUp.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React from "react";
+import "bulma/css/bulma.min.css";
+import "./SignUp.css"; // Tambahkan file CSS custom jika diperlukan
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="hero is-fullheight is-light">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+              <form className="box">
+                <h1 className="title has-text-centered">Sign Up</h1>
+                <div className="field">
+                  <label className="label">Name</label>
+                  <div className="control has-icons-left">
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="Enter your name"
+                      required
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-user"></i>
+                    </span>
+                  </div>
+                </div>
 
-export default SignUp
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control has-icons-left">
+                    <input
+                      type="email"
+                      className="input"
+                      placeholder="Enter your email"
+                      required
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-envelope"></i>
+                    </span>
+                  </div>
+                </div>
+
+                <div className="field">
+                  <label className="label">Password</label>
+                  <div className="control has-icons-left">
+                    <input
+                      type="password"
+                      className="input"
+                      placeholder="Enter your password"
+                      required
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-lock"></i>
+                    </span>
+                  </div>
+                </div>
+
+                <div className="field">
+                  <label className="label">Confirm Password</label>
+                  <div className="control has-icons-left">
+                    <input
+                      type="password"
+                      className="input"
+                      placeholder="Confirm your password"
+                      required
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-lock"></i>
+                    </span>
+                  </div>
+                </div>
+
+                <div className="field">
+                  <button className="button is-primary is-fullwidth">Sign Up</button>
+                </div>
+
+                <p className="has-text-centered">
+                  Already have an account? <a href="#">Login here</a>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
