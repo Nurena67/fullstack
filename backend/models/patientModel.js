aa2ea7276525f0b2bb5fd8a0895e3bef0fb6169a
M R N Al -Irsyad <arisnurena@gmail.com>
2024-12-24 11:38:31 +0700

    Update patientModel.js

--- backend/models/patientModel.js
+++ backend/models/patientModel.js
@@ -39,14 +39,6 @@ const Patient = sequelize.define(
       type: Sequelize.STRING,
       allowNull: true,
     },
-    userId: {
-      type: Sequelize.UUID,
-      allowNull: false,
-      references: {
-        model: 'User', // Nama tabel yang dituju
-        key: 'uuid',
-      },
-    },
   },
   {
     timestamps: true,
